40819091310a19bc61e7a56d8c0f4a78075380cb
Alexys Martinez GOMEZ <alexys.martinez@qualud.com>
2025-06-04 13:04:30 +0200

    select fixed

---
+++ src/app/components/whatsapp-widget/whatsapp-widget.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import {NgIf} from "@angular/common";
+
+@Component({
+  selector: 'app-whatsapp-widget',
+  imports: [
+    NgIf
+  ],
+  templateUrl: './whatsapp-widget.component.html',
+  styleUrl: './whatsapp-widget.component.css'
+})
+export class WhatsappWidgetComponent {
+  isOpen = false;
+
+  toggleChat() {
+    this.isOpen = !this.isOpen;
+  }
+
+  get whatsappLink(): string {
+    const phone = '34653539081'; // Sin +
+    const message = encodeURIComponent('Hola, me gustaría más información');
+    return `https://wa.me/${phone}?text=${message}`;
+  }
+}
